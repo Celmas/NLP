7cd6e744e2f391ca6328ab32bc6c40042c81a015
BuildTools <unconfigured@null.spigotmc.org>
2020-03-17 22:13:29 +0300

    added comments

--- lesson3_logicsticregresion/logistic.py
+++ lesson3_logicsticregresion/logistic.py
@@ -8,7 +8,7 @@ from sklearn.metrics import precision_recall_fscore_support
 
 def get_normal_form(text):
     analyzer = pymorphy2.MorphAnalyzer()
-    puncto = [',', '.', ':', '?', '«', '»', '-', '(', ')', '!', '\'', '—', ';', '”', '...', '#', '$', '%']
+    puncto = [',', '.', ':', '?', '«', '»', '-', '(', ')', '!', '\'', '—', ';', '”', '...', '#', '$', '%', '&']
     tokens = nltk.word_tokenize(text)
     normalized_words = []
     for token in tokens:
@@ -42,21 +42,29 @@ def filter_by_reviews_title(data_frame, reviews_titles):
     return filtered
 
 
+# Считываем все отзывы
 df = pandas.read_csv("reviews.csv", encoding="utf-8")
 my_reviews = ["Гладиатор", "Начало", "Помни"]
+
+# Отделяем тестовые отзывы
 test_data = df[df['title'].isin(my_reviews)]
 df = filter_by_reviews_title(df, my_reviews)
 
+# Получаем словарь всех слов и представление текстов в виде мешка слов
 vocabulary, bag_of_words = get_vocab_and_bag_of_words_vectors(df.values, [])
+# Подготавливаем тестовое представление текстов
 vocabulary, test_bag_of_words = get_vocab_and_bag_of_words_vectors(test_data.values, vocabulary)
 
+# Тренируем модель
 print("---Train---")
 reg = LogisticRegression(max_iter=10000)
 reg.fit(bag_of_words, df['label'])
 
+# Применяем модель к текстовым текстам
 print("---Predict---")
 predicted = reg.predict(test_bag_of_words)
 
+# Находим Accuracy
 true_positives = 0
 k = 0
 for prediction in predicted:
@@ -65,15 +73,18 @@ for prediction in predicted:
     k += 1
 
 print("Accuracy: {}".format(true_positives / len(predicted)))
+# Находим Precision, Recall, Fscore
 precision_recall_fscore = precision_recall_fscore_support(test_data['label'].values, predicted)
 print(f"Precision(-1, 0, 1) = {precision_recall_fscore[0]}")
 print(f"Recall(-1, 0, 1) = {precision_recall_fscore[1]}")
 print(f"Fscore(-1, 0, 1) = {precision_recall_fscore[2]}")
 
+# Создаем словари соотнося все слова и коэффициенты для кадого типа отзывов
 dict_of_negative = dict(zip(vocabulary, reg.coef_[0]))
 dict_of_neutral = dict(zip(vocabulary, reg.coef_[1]))
 dict_of_positive = dict(zip(vocabulary, reg.coef_[2]))
 
+# Соритруем
 sorted_negative_dictionary = {k: v for k, v in sorted(dict_of_negative.items(), key=lambda item: item[1], reverse=True)}
 sorted_neutral_dictionary = {k: v for k, v in sorted(dict_of_neutral.items(), key=lambda item: item[1], reverse=True)}
 sorted_positive_dictionary = {k: v for k, v in sorted(dict_of_positive.items(), key=lambda item: item[1], reverse=True)}
